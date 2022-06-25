5a6ce741544ea45bdb22d423078c0929782d7294
parsaakbari1209 <parsaakbari1209@gmail.com>
2022-06-25 14:58:01 +0430

    feat: add github actions for continues delivery

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: '/main',
   reactStrictMode: true,
+  rewrites() {
+    return [{ source: '/main/_next/:path*', destination: '/_next/:path*' }]
+  },
 }
 
 module.exports = nextConfig
