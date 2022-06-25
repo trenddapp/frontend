0d4dcd29901f1d7749a8320090072ea79a918d6d
parsaakbari1209 <parsaakbari1209@gmail.com>
2022-06-25 12:46:11 +0430

    feat: add toolkit components

---
+++ src/components/Toolkit/Flex.js
@@ -0,0 +1,11 @@
+import { flexbox } from 'styled-system'
+import styled from 'styled-components'
+
+import Box from './Box'
+
+const Flex = styled(Box)`
+  display: flex;
+  ${flexbox}
+`
+
+export default Flex
