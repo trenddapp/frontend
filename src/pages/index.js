2e552831f5698dd798e8a99ebfe7c2a76b473bb5
parsaakbari1209 <parsaakbari1209@gmail.com>
2022-06-25 14:45:56 +0430

    feat: add coming soon page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,31 @@
 import Head from 'next/head'
 
+import styled, { useTheme } from 'styled-components'
+
+import { Flex, Text } from '../components/Toolkit'
+
+const Container = styled(Flex)`
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  margin: 0 auto;
+  max-width: ${({ theme }) => `${theme.siteWidth}px`};
+  padding: 8px;
+
+  ${({ theme }) => theme.mediaQueries.sm} {
+    border-left: 1px dashed ${({ theme }) => theme.colors.borderAlt};
+    border-right: 1px dashed ${({ theme }) => theme.colors.borderAlt};
+  }
+`
+
+const Section = styled.section`
+  background-color: ${({ theme }) => theme.colors.background};
+  padding: 0 16px;
+`
+
 const HomePage = () => {
+  const theme = useTheme()
+
   return (
     <>
       <Head>
@@ -8,6 +33,13 @@ const HomePage = () => {
         <meta name="description" content="Decentralized applications by TrendDAPP." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Section>
+        <Container>
+          <Text color={theme.colors.text} fontSize="52px" fontWeight="500">
+            Coming Soon!
+          </Text>
+        </Container>
+      </Section>
     </>
   )
 }
