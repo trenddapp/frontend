cace3fbf4e6cc39b373dfc105de0eebac4f3ebcb
parsaakbari1209 <parsaakbari1209@gmail.com>
2022-07-05 01:45:12 +0430

    feat: add background image to hero section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,17 @@
 import Head from 'next/head'
 
-import styled from 'styled-components'
+import styled, { useTheme } from 'styled-components'
 
-import { Flex, Text } from '../components/Toolkit'
+import { Box, Flex, Text } from '../components/Toolkit'
 import { Nav } from '../components/Nav'
 
 const Button = styled.button`
-  background-color: ${({ theme }) => theme.colors.action};
+  background-color: rgba(14, 19, 24, 0.7);
   border-radius: ${({ theme }) => theme.radii.small};
   border: none;
   color: ${({ theme }) => theme.colors.background};
   font-family: Avenir;
-  margin-top: 40px;
+  margin-top: 65px;
   padding: 12px;
 
   &:hover {
@@ -24,12 +24,22 @@ const Container = styled(Flex)`
   height: 100vh;
   margin: 0 auto;
   max-width: ${({ theme }) => `${theme.siteWidth}px`};
-  padding: 8px;
+  position: relative;
+`
 
-  ${({ theme }) => theme.mediaQueries.sm} {
-    border-left: 1px dashed ${({ theme }) => theme.colors.borderAlt};
-    border-right: 1px dashed ${({ theme }) => theme.colors.borderAlt};
-  }
+const Background = styled(Box)`
+  background-color: rgb(20, 92, 146);
+  background-image: url('/main/wave.webp');
+  background-position-x: center;
+  background-position-y: bottom;
+  background-repeat: no-repeat;
+  bottom: 0;
+  display: block;
+  height: 880px;
+  left: 0;
+  position: absolute;
+  right: 0;
+  top: 70px;
 `
 
 const Image = styled.img`
@@ -42,6 +52,8 @@ const Section = styled.section`
 `
 
 const HomePage = () => {
+  const theme = useTheme()
+
   return (
     <>
       <Head>
@@ -49,48 +61,56 @@ const HomePage = () => {
         <meta name="description" content="Decentralized applications by TrendDapp." />
         <link rel="icon" href="/main/favicon.ico" />
       </Head>
+
       <Nav />
       <Section>
+        <Background />
         <Container>
-          {/* First section which includes two sides in a row. */}
-          <Flex justifyContent="space-evenly" marginTop="100px" width="100%">
-            {/* Left side which includes three sides in a column.  */}
+          <Flex justifyContent="space-between" padding="120px 0 0 0" width="100%">
             <Flex flexDirection="column">
               <Flex flexDirection="column">
-                <Text fontFamily="Avenir" fontSize="65px" fontWeight="700" lineHeight="65px">
+                <Text
+                  color={theme.colors.background}
+                  fontFamily="Avenir"
+                  fontSize="65px"
+                  fontWeight="700"
+                  lineHeight="65px"
+                >
                   We Know
                 </Text>
-                <Text fontFamily="Avenir" fontSize="65px" fontWeight="700" lineHeight="65px">
+                <Text
+                  color={theme.colors.background}
+                  fontFamily="Avenir"
+                  fontSize="65px"
+                  fontWeight="700"
+                  lineHeight="65px"
+                >
                   Blockchain.
                 </Text>
               </Flex>
-              <Flex flexDirection="column" marginTop="40px">
-                <Text fontFamily="Avenir" fontSize="20px">
+              <Flex flexDirection="column" margin="25px 0 0 0">
+                <Text color={theme.colors.background} fontFamily="Avenir" fontSize="20px">
                   Turn your idea for a decentralized application, smart contract,
                 </Text>
-                <Text fontFamily="Avenir" fontSize="20px">
+                <Text color={theme.colors.background} fontFamily="Avenir" fontSize="20px">
                   crypto wallet, or non-fungible token into a real business.
                 </Text>
               </Flex>
               <Button>Let's discuss your project</Button>
             </Flex>
-
-            {/* Right side which includes one image. */}
             <Flex height="326px">
               <Image src="/main/cosmic-ray-detector.png" />
             </Flex>
           </Flex>
-
-          {/* Second section which includes one rounding row. */}
-          <Flex justifyContent="space-around" marginTop="100px">
-            <img title="Bitcoin" src="/main/bitcoin.png" height="50px" />
-            <img title="Ethereum" src="/main/ethereum.png" height="50px" />
-            <img title="Polygon" src="/main/polygon.png" height="50px" />
-            <img title="Solidity" src="/main/solidity.png" height="50px" />
-            <img title="Metamask" src="/main/metamask.png" height="50px" />
-            <img title="Filecoin" src="/main/filecoin.png" height="50px" />
-            <img title="Golang" src="/main/golang.png" height="50px" />
-            <img title="Nextjs" src="/main/nextjs.png" height="50px" />
+          <Flex justifyContent="space-around" padding="300px 0 0 0">
+            <img height="50px" title="Bitcoin" src="/main/bitcoin.png" />
+            <img height="50px" title="Ethereum" src="/main/ethereum.png" />
+            <img height="50px" title="Polygon" src="/main/polygon.png" />
+            <img height="50px" title="Solidity" src="/main/solidity.png" />
+            <img height="50px" title="Metamask" src="/main/metamask.png" />
+            <img height="50px" title="Filecoin" src="/main/filecoin.png" />
+            <img height="50px" title="Golang" src="/main/golang.png" />
+            <img height="50px" title="Nextjs" src="/main/nextjs.png" />
           </Flex>
         </Container>
       </Section>
