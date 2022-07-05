7004e561782dff2a79442dfbdcae34d30d8b4adf
ghader salehi <salehi.ghader01@gmail.com>
2022-07-05 13:52:34 +0430

    feat: add tailwind to project

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 import { ThemeProvider } from 'styled-components'
-
 import { GlobalStyle, ResetCss } from '../styles'
+import '../styles/tailwind.css'
 import { light } from '../themes'
 
 const App = ({ Component, pageProps }) => {
