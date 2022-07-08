3d033d35cb23f4be2c21eb5b2b4830ef196a58f6
parsaakbari1209 <parsaakbari1209@gmail.com>
2022-07-08 11:01:38 +0430

    chore: add styled-components to nextjs compiler options

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   assetPrefix: '/main',
+  compiler: {
+    styledComponents: true,
+  },
   reactStrictMode: true,
   rewrites() {
     return [{ source: '/main/_next/:path*', destination: '/_next/:path*' }]
