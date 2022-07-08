fbe4b739e9c640bc910600c10b10e80ffd6a0b44
parsaakbari1209 <parsaakbari1209@gmail.com>
2022-07-08 18:57:18 +0430

    feat: add trusted by section

--- src/components/layout/Main/Main.js
+++ src/components/layout/Main/Main.js
@@ -1,11 +1,13 @@
 import Advantages from './Advantages'
 import Hero from './Hero'
+import TrustedBy from './TrustedBy'
 
 const Main = () => {
   return (
     <main>
       <Hero />
       <Advantages />
+      <TrustedBy />
     </main>
   )
 }
