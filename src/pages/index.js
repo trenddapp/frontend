239352b7906837bc9f4b168b8bc721ff26eae1fd
ghader salehi <salehi.ghader01@gmail.com>
2022-07-08 22:04:57 +0430

    feat: add footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 
-import { Footer } from '@/components/layout/Footer'
+import Footer from '@/components/layout/Footer'
 import { Header } from '@/components/layout/Header'
 import { Main } from '@/components/layout/Main'
 
