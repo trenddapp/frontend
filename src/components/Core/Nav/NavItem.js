51506e5f90cbfe11dd78ddc29d96a5c9a2c089ba
parsaakbari1209 <parsaakbari1209@gmail.com>
2022-07-19 00:21:18 +0430

    feat: make header responsive on mobile

--- src/components/Core/Nav/NavItem.js
+++ src/components/Core/Nav/NavItem.js
@@ -3,50 +3,45 @@ import Link from 'next/link'
 
 import styled from 'styled-components'
 
-import { SvgSolidChevronDown } from '@/components/Svg'
+const StyledNavItem = styled.li`
+  border-top: 1px solid rgb(216, 216, 216);
+  color: #282846;
+  font-size: 13px;
+  font-weight: 500;
+  list-style: none;
+  padding: 32px 0;
 
-const StyledNavIcon = styled.i`
-  margin: -4px 0 0 12px;
+  ${({ theme }) => theme.mediaQueries.md} {
+    align-items: center;
+    border: none;
+    cursor: pointer;
+    display: flex;
+    font-size: 15px;
+    line-height: 45px;
+    margin: 0 40px 0 0;
+    padding: 0;
+    transition: color 0.4s ease;
+
+    &:hover {
+      color: #614dce;
+    }
+
+    &:last-child {
+      margin: 0;
+    }
+  }
 `
 
-const StyledNavItem = styled.li`
-  align-items: center;
-  color: #282846;
+const StyledNavLink = styled.a`
   cursor: pointer;
-  display: flex;
-  font-size: 15px;
-  font-weight: 500;
-  line-height: 45px;
-  list-style: none;
-  margin: 0 40px 0 0;
-  transition: color 0.4s ease;
-
-  &:hover {
-    color: #614dce;
-  }
-
-  &:last-child {
-    margin: 0;
-  }
 `
 
 const NavItem = forwardRef(({ children, link, ...rest }, ref) => {
-  if (link != '') {
-    return (
-      <StyledNavItem ref={ref} {...rest}>
-        <Link href={link}>
-          <a>{children}</a>
-        </Link>
-      </StyledNavItem>
-    )
-  }
-
   return (
     <StyledNavItem ref={ref} {...rest}>
-      {children}
-      <StyledNavIcon>
-        <SvgSolidChevronDown />
-      </StyledNavIcon>
+      <Link href={link}>
+        <StyledNavLink>{children}</StyledNavLink>
+      </Link>
     </StyledNavItem>
   )
 })
