91560129cbfc425761095f457e792e05490d3f9c
parsaakbari1209 <parsaakbari1209@gmail.com>
2022-08-18 15:30:54 +0430

    chore(lottery): remove the next draw section until it's done

--- src/components/View/Lottery/index.js
+++ src/components/View/Lottery/index.js
@@ -3,14 +3,13 @@ import ClaimReward from './components/ClaimReward'
 import Header from './components/Header'
 import Hero from './components/Hero'
 import History from './components/History'
-import NextDraw from './components/NextDraw'
 
 const Lottery = () => {
   return (
     <Box>
       <Header />
       <Hero />
-      <NextDraw />
+      {/* TODO: Add next draw section. */}
       <ClaimReward />
       <History />
     </Box>
