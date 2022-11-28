354451f06eb8ca6818294ccafb70a4f213edc5a0
parsaakbari1209 <parsaakbari1209@gmail.com>
2022-11-28 23:48:01 +0330

    feat: add wip component

--- src/app/company/assets/page.tsx
+++ src/app/company/assets/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { Wip } from 'lib/component/Wip'
+
 export default function Page() {
-  return <>Assets Page</>
+  return <Wip />
 }
--- src/app/dapps/auction/page.tsx
+++ src/app/dapps/auction/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { Wip } from 'lib/component/Wip'
+
 export default function Page() {
-  return <>Auction Page</>
+  return <Wip />
 }
