7f34d00acbc3fb73a20c7f5d5fdc5001dda113ed
parsaakbari1209 <parsaakbari1209@gmail.com>
2023-01-03 23:46:08 +0330

    chore: fix styled-components initial load problem

--- src/lib/style/index.tsx
+++ src/lib/style/index.tsx
@@ -1,2 +1,3 @@
 export { default as GlobalCss } from './GlobalCss'
 export { default as ResetCss } from './ResetCss'
+export { default as StyledComponentsRegistry } from './StyledComponentsRegistry'
