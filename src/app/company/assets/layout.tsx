ffe642646ea2abe3e7c7b9c96c2abe65cd46e279
parsaakbari1209 <parsaakbari1209@gmail.com>
2023-03-16 20:48:32 +0330

    chore: remove useless pages

--- src/app/company/assets/layout.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-interface LayoutProps {
-  children: React.ReactNode
-}
-
-export default function Layout(props: LayoutProps) {
-  return props.children
-}
--- src/app/company/careers/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-
-import { Wip } from 'lib/component/Wip'
-
-export default function Page() {
-  return <Wip />
-}
